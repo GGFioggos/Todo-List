9fee551beca50efe71b84f1047d638398cefb4f3
moschidisg <mosxidismosxidis@gmail.com>
2022-09-29 14:23:58 +0300

    added default projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import { setUpSidebar } from "./sidebar";
+import { setUpContent } from "./content";
+import { Project } from "./content";
+import { Task } from "./content";
+
+
+export let projects = createDefaultProjects();
 
 setUpSidebar();
+setUpContent();
+
+function createDefaultProjects() {
+	const todayProject = Project("Today", "");
+	todayProject.projectTasks = [
+		Task("Groceries", "Get groceries form the supermarket"),
+		Task("Homework", "Do the homework"),
+	];
+
+	const thisWeekProject = Project("This Week", "");
+	thisWeekProject.projectTasks = [
+		Task("Finish CS project", ""),
+		Task("Service car", "Service the car to Tomas' service shop"),
+	];
+
+	const thisMonthProject = Project("This Month", "");
+	thisMonthProject.projectTasks = [
+		Task("Pay Electricity Bills", "Pay bills online"),
+		Task("Search for new job", ""),
+	];
+
+	return [todayProject, thisWeekProject, thisMonthProject];
+}
